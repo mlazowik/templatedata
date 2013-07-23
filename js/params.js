mlazowik/templatedata

commit d28336a667973bd3a207ce8f72bd369e8e8d69f3

params.js: Some jQuery weirdness

js/params.js
@@ -137,7 +137,7 @@ Backgrid.AliasesCell = Backgrid.Extension.TextCell.extend({
 		var that = this;
 		$.each(names, function(index, value) {
 			if ( value != "" )
-				that.$el.append("<code>" + $('<div/>').text(value).html() + "</code> ");
+				that.$el.append($('<code/>').append($('<div/>').text(value)));
 			if (hr) {
 				that.$el.append("<hr />");
 				hr = false;
@@ -381,7 +381,7 @@ function templatedata($collection, $description) {
 
 	data["params"] = params;
 
-	$("[id='templatedata']").html( JSON.stringify( data, null, "\t" ) );
+	$("[id='templatedata']").text( JSON.stringify( data, null, "\t" ) );
 }
 
 $(document).ready( function() {

commit 7f357b263e0166a34f5a720e0607b015c1e2a3ba

params.js: Some jQuery weirdness, attempt two
I broke the code in d28336a667973bd3a207ce8f72bd369e8e8d69f3.
This fixes it back.

js/params.js
@@ -137,7 +137,7 @@ Backgrid.AliasesCell = Backgrid.Extension.TextCell.extend({
 		var that = this;
 		$.each(names, function(index, value) {
 			if ( value != "" )
-				that.$el.append($('<code/>').append($('<div/>').text(value)));
+				that.$el.append($('<code/>').text(value), ' ');
 			if (hr) {
 				that.$el.append("<hr />");
 				hr = false;