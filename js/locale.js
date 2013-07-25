6919172996ee420eff0a00cd43af20f17d5751c1
Michał Łazowik <mlazowik@gmail.com>
2013-07-25 20:04:12 +0200

    Get base language from extended (e.g. "nl-NL").

--- js/locale.js
+++ js/locale.js
@@ -10,7 +10,9 @@ var options = {
 $.i18n.init( options, function(t) {
 	$("body").i18n();
 	$("#desc").attr( "placeholder", t("placeholders.desc") );
-	$("#lang-select").val( $.i18n.lng() );
+
+	var lang = $.i18n.lng().split("-");
+	$("#lang-select").val( lang[0] );
 
 	$("#lang-select").change( function() {
 		document.location.href = '?lang=' + $(this).val();
